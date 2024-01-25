90243ee8b0c41969c97b887efa80783d1ec80979
Maxim <112863178+grilld9@users.noreply.github.com>
2024-01-25 14:45:38 +0600

    init commit.

---
+++ src/main/java/ru/maksimenkov/techLo/TechLoApplication.java
@@ -0,0 +1,13 @@
+package ru.maksimenkov.techLo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maksimenkov/techLo/TechLoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maksimenkov.techLo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
